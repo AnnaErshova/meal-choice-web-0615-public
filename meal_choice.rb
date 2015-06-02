56d47d5112d6aa8e2eb77bc9422426eed7ccd96f
AnnaErshova <anna.ershova@gmail.com>
2015-06-02 14:19:48 -0400

    all done

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,10 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-puts "Enter your meal choice, please"
-name = gets.chomp
-name.downcase!
-
 def meal_choice(name="meat")
     return name
 end
